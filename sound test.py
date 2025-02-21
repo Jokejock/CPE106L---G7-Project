6fbfe653f4eb3cbe7cb0ab8b7ed7c1a06c52622e
Jokejock <86518565+Jokejock@users.noreply.github.com>
2025-02-22 02:28:47 +0800

    sound test
    for alarm:
    note:
    install pip and pygame for the sounds to worl

--- sound test.py
+++ sound test.py
@@ -6,7 +6,7 @@ pygame.mixer.init()
 
 # Load your sound file (make sure to have a .wav or .mp3 file in the same directory)
 sound_file = "your_sound_file.wav"  # Replace with your sound file
-pygame.mixer.music.load(sound_file)
+pygame.mixer.music.load('amongsus.mp3')
 
 def play_sound():
     pygame.mixer.music.play()
